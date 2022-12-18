6eb7616c6f9d9efd019d02814d6dbce1bba1cff0
baoanh16 <vanhoangbao@gmail.com>
2022-12-18 23:32:50 +0700

    First version

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+console.log(`Van Hoang Bao
+Mollit sunt duis mollit est occaecat. Culpa pariatur culpa proident ullamco do. Occaecat ipsum sunt dolore sit ad et eu est veniam. Magna consequat consequat deserunt cupidatat deserunt excepteur quis aliqua voluptate tempor consectetur deserunt.
+
+Nisi est exercitation velit eu ea eu dolore anim Lorem elit sit magna aliquip dolore. Cupidatat est irure eu aliquip commodo elit amet velit quis occaecat do. Aliquip in ea deserunt labore dolor.
+
+Nostrud laborum voluptate duis pariatur est pariatur mollit. Occaecat laboris occaecat ex exercitation nisi minim adipisicing adipisicing esse veniam. Nisi quis dolore reprehenderit nulla aute do id laborum nostrud eu ipsum sunt laboris. Proident officia ipsum duis labore consequat minim dolore ullamco elit irure minim excepteur. Voluptate id labore esse ullamco. Laborum id consectetur aute cupidatat elit esse.
+
+Est fugiat mollit aliqua veniam elit culpa consectetur sint ullamco nulla aute officia. Mollit sunt ea amet id exercitation in pariatur officia in nisi anim eiusmod nostrud reprehenderit. Aliqua ipsum anim ut consectetur cupidatat commodo cillum cupidatat Lorem velit nostrud anim. Culpa mollit officia non sunt sint eu aliquip quis.
+
+Consequat eu laborum ea elit non sit occaecat nisi culpa culpa non nisi amet. Ut nulla incididunt aute laborum exercitation irure magna nulla ex dolore est. Nisi ut adipisicing ut enim.
+
+⚙ https://github.com/baoanh16
+
+`);
